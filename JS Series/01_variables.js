25b8446d143c3f5457490abc42f5b8dd1e509866
Musfik1130 <musfikhasnain8@gmail.com>
2026-03-08 23:04:39 +0600

    Add files via upload

---
+++ JS Series/01_variables.js
@@ -0,0 +1,9 @@
+console.log("we will learn here:- let const var");
+const AccountId = 1012;
+// const AccountId = 1324; not possible, Const is a locked value;
+let AccountName = "Musfik"
+var AccountPassword = "1232@aro"
+important = "we dont' use Var because of Blocked Scope problem"
+
+console.table([AccountId, AccountName, AccountPassword]);
+console.log(important)
---
+++ JS Series/Data_types.js
@@ -0,0 +1,18 @@
+// "Js history, strick rule" treat all JS code as newer version
+
+// alert(3+3)  //it's not possible bcz we are using Node.js not browser(possible in browser)
+
+let string = "Musfik"
+let number = 1223
+let boolean = true;
+let nullvalue = null; //standalone vallue
+let undefinedValue; 
+
+// bigint = over->2p53
+//symbol = unique value;
+
+console.log(nullvalue, "is a", typeof nullvalue);
+console.log(number, "is a", typeof number);
+console.log(string,"is a", typeof string);
+console.log(undefinedValue,"is a", typeof undefinedValue);
+console.log(boolean,"is a", typeof boolean);
